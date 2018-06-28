8c2dc805e272e9fbf262c8094437b7f3f6ec73df
michal <orlinski.michal.it@gmail.com>
2018-06-28 12:11:16 +0200

    Import projektu Spring Boot

---
+++ src/main/java/pl/morlinski/wjug/ExampleWjugSpringBootApplication.java
@@ -0,0 +1,12 @@
+package pl.morlinski.wjug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWjugSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleWjugSpringBootApplication.class, args);
+    }
+}
